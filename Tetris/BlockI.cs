b85c90d60652e05714a67693be69ad227baaeda9
Daisy <xinjie.d@gmail.com>
2016-06-11 00:09:36 +0800

    c# DEMO

---
+++ Tetris/BlockI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class BlockI:Block
+    {
+
+        public BlockI()
+        {
+            PenColor = Color.Black;
+            BrushColor = Color.Blue;
+
+            Shape[0] = Shape[2] = new int[,]{
+                {1,1,1,1}
+            };
+
+            Shape[1] = Shape[3] = new int[,]{
+                {1},
+                {1},
+                {1},
+                {1}
+            };
+        }
+    }
+}
---
+++ Tetris/BlockO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class BlockO:Block
+    {
+
+
+        public BlockO()
+        {
+            PenColor = Color.Black;
+            BrushColor = Color.Orange;
+
+            Shape[0] = Shape[1] = Shape[2] = Shape[3] = new int[,]{
+                {1,1},
+                {1,1}
+            };
+        }
+
+
+    }
+}
